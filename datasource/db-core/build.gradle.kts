444d145f4f9452a265d457b6f79b93f48d0fddc4
alen.heo <hubtwork@gmail.com>
2023-11-30 00:33:51 +0900

    modify gradle & db-core.yml

--- datasource/db-core/build.gradle.kts
+++ datasource/db-core/build.gradle.kts
@@ -6,9 +6,5 @@ plugins {
 dependencies {
     compileOnly(project(":domain"))
 
-    runtimeOnly(libs.db.h2)
-
-    implementation("com.linecorp.kotlin-jdsl:jpql-dsl:3.0.0")
-    implementation("com.linecorp.kotlin-jdsl:jpql-render:3.0.0")
-    implementation("com.linecorp.kotlin-jdsl:spring-data-jpa-support:3.0.0")
+    implementation(libs.db.h2)
 }
