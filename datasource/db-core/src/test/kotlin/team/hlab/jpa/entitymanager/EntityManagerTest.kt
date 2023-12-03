597e9c49da26aac9e31397f1f47aab79013c5eb9
alen.heo <hubtwork@gmail.com>
2023-12-03 17:43:53 +0900

    add transaction rollback test

--- datasource/db-core/src/test/kotlin/team/hlab/jpa/entitymanager/EntityManagerTest.kt
+++ datasource/db-core/src/test/kotlin/team/hlab/jpa/entitymanager/EntityManagerTest.kt
@@ -1,9 +1,14 @@
 package team.hlab.jpa.entitymanager
 
+import jakarta.persistence.EntityManager
 import jakarta.persistence.EntityManagerFactory
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import team.hlab.jpa.SpringJpaTest
+import team.hlab.jpa.member.MemberEntity
+import team.hlab.jpa.support.transactional
+import java.util.UUID
 
 @SpringJpaTest
 class EntityManagerTest(
@@ -13,15 +18,23 @@ class EntityManagerTest(
     @Test
     fun t1() {
         val em = entityManagerFactory.createEntityManager()
-        val tx = em.transaction
-        try {
-            tx.begin()
-            // logic
-            tx.commit()
-        } catch (e: Throwable) {
-            tx.rollback()
-        } finally {
-            em.close()
+        assertThat(em).isInstanceOf(EntityManager::class.java)
+    }
+
+    @DisplayName("when error thrown, transaction must rollback")
+    @Test
+    fun t2() {
+        val id = UUID.randomUUID().toString()
+
+        val em = entityManagerFactory.createEntityManager()
+        em.transactional {
+            val member = MemberEntity(id = id, username = "alen", age = 1, address = "hlab")
+            em.persist(member)
+            throw Exception("something wrong")
         }
+
+        val em2 = entityManagerFactory.createEntityManager()
+        val nullMember = em2.find(MemberEntity::class.java, id)
+        assertThat(nullMember).isNull()
     }
 }
