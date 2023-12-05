5373f3606f16d0e3183ff40a9933468b4881807f
alen.heo <hubtwork@gmail.com>
2023-12-05 20:00:13 +0900

    add MemberAdminEntity.kt for master-table

---
+++ datasource/db-core/src/main/kotlin/team/hlab/jpa/member/MemberAdminEntity.kt
@@ -0,0 +1,41 @@
+package team.hlab.jpa.member
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Entity
+@Table(name = "MEMBER")
+class MemberAdminEntity(
+    @Id
+    @Column(name = "ID")
+    val id: String,
+    @Column(name = "NAME")
+    val username: String,
+    @Column(name = "AGE")
+    val age: Int,
+    @Column(name = "ADDRESS")
+    val address: String,
+    @Enumerated(EnumType.STRING)
+    @Column(name = "GENDER")
+    val gender: Gender,
+    @Column(name = "EMAIL")
+    val email: String,
+) {
+    override fun toString(): String {
+        return "id: $id, username: $username, age: $age, email: $email, gender: $gender, address: $address"
+    }
+
+    enum class Gender {
+        MALE,
+        FEMALE,
+    }
+}
+
+@Repository
+interface MemberAdminJpaRepository : JpaRepository<MemberAdminEntity, String>
